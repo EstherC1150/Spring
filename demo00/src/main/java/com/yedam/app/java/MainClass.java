5a841d1c0bac00b5658986d7135ba237901d60aa
다예 <122209197+EstherC1150@users.noreply.github.com>
2024-01-12 17:05:09 +0900

    240112_springStart중간푸시커밋밋

---
+++ demo00/src/main/java/com/yedam/app/java/MainClass.java
@@ -0,0 +1,10 @@
+package com.yedam.app.java;
+
+public class MainClass {
+	public static void main(String[] args) {
+//		동작하는 코드를 건드려야 한다
+//		TV tv = new LGTV();
+		TV tv = new SamsungTV();
+		tv.on();
+	}
+}
